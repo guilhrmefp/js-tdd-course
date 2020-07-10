6cc567a7c3d7ff8ad82668790ddf74ee324a3aa6
Guilherme Fernandes Pereira <guilherme.pereira@ventron.com.br>
2020-07-09 21:52:29 -0300

    76. Funcionamento do Mocha - describe, context, it...

---
+++ tests/main.spec.js
@@ -0,0 +1,20 @@
+// describes a main block of tests
+describe('Main', () => {
+  // you can have multiple describes ex: one for each method
+  describe('Method A', () => {
+    // the context of the test
+    context('Case 1', () => {
+      // the test itself
+      it('should happen blabla', () => {
+
+      });
+    });
+
+    context('Case 2', () => {
+
+    });
+  });
+
+  describe('Method B', () => {
+  });
+});
