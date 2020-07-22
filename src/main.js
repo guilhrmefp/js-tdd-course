08880c491ba020b68aa0f7b6675860d2fadc9c27
Guilherme Fernandes Pereira <guilherme.pereira@ventron.com.br>
2020-07-21 22:50:20 -0300

    83. Criando uma calculadora simples

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 module.exports = {
-  sum: () => {},
-  sub: () => {},
-  multi: () => {},
-  div: () => {},
+  sum: (num1, num2) => num1 + num2,
+  sub: (num1, num2) => num1 - num2,
+  multi: (num1, num2) => num1 * num2,
+  div: (num1, num2) => (num2 === 0 ? 'Can not divide by zero' : num1 / num2),
 };
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -32,4 +32,36 @@ describe('Calc', () => {
       expect(calc.div).to.be.a('function');
     });
   });
+
+  describe('Sum', () => {
+    it('should return 4 when `sum(2, 2)`', () => {
+      expect(calc.sum(2, 2)).to.be.equal(4);
+    });
+  });
+
+  describe('Sub', () => {
+    it('should return 4 when `sub(6, 2)`', () => {
+      expect(calc.sub(6, 2)).to.be.equal(4);
+    });
+
+    it('should return -4 when `sub(6, 10)`', () => {
+      expect(calc.sub(6, 10)).to.be.equal(-4);
+    });
+  });
+
+  describe('Multi', () => {
+    it('should return 4 when `multi(2, 2)`', () => {
+      expect(calc.multi(2, 2)).to.be.equal(4);
+    });
+  });
+
+  describe('Div', () => {
+    it('should return 2 when `div(4, 2)`', () => {
+      expect(calc.div(4, 2)).to.be.equal(2);
+    });
+
+    it('should return `Can not divide by zero` when divide by 0', () => {
+      expect(calc.div(4, 0)).to.be.equal('Can not divide by zero');
+    });
+  });
 });
