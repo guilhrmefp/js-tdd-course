791abb97c3c24c033c5787fd56b428e1cf70322d
Guilherme Fernandes Pereira <guilherme.pereira@ventron.com.br>
2020-08-03 21:18:53 -0300

    90. Criando Smoke Tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,12 +1,36 @@
 import chai, { expect } from 'chai';
 import dirtyChai from 'dirty-chai';
 
+import {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+} from '../src/main';
+
 chai.use(dirtyChai);
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
   describe('Smoke tests', () => {
-    it('should exist', () => {
-      expect(chai).to.exist();
+    it('should exist the search method', () => {
+      expect(search).to.exist();
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist();
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist();
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist();
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist();
     });
   });
 });
